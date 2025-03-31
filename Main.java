9899272f1edc15781c31d07d0e9310ae0d4ec5ac
CalebChun <1024656@oside.us>
2025-03-31 15:12:13 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,16 +7,21 @@ public class Main
 	{
 
 		/***** DECLARATION SECTION *****/
-	int days = 2, month = 2, year = 2002;
+	int dayToday = 2, monthToday = 2, yearToday = 2020;
+	int dayBday = 8, monthBday = 26, yearBday = 1918;
 		/***** INITIALIZATION SECTION *****/
 		
 		/***** INPUT SECTION *****/
 		// N/A (no input for this lab)
 		
 		/***** CALCULATION & PROCESSING SECTION *****/
-int jdn = days + (153*month+2)/5 + 365*year + year*0.25 - year*0.01 + year/400 - 32045;
+int jdnToday = dayToday + (153*monthToday+2)/5 + 365*yearToday + yearToday*0.25 - yearToday*0.01 + yearToday/400 - 32045;
+int jdnBday = dayBday + (153*monthBday+2)/5 + 365*yearBday + yearBday*0.25 - yearBday*0.01 + yearBday/400 - 32045;
 		
 		/***** OUTPUT SECTION *****/
-		System.out.println("Julian Date Number: " + jdn);
+		System.out.println("Julian Day Number for today's date, 2/2/2020, is" + jdnToday);
+		System.out.println("Julian Day Number for birthday, 8/26/1918, is" + jdnBday);
+		System.out.println("The difference in days is " + jdnBday + " which makes you approximately 101 years old!")
+		
   }
 }
