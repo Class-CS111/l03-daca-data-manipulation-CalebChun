82b4a071dcf5797143ee58a9482b2d7e49bb15d4
CalebChun <1024656@oside.us>
2025-03-31 15:01:12 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
-// STUDENT NAME:
-// DATE: 
+// STUDENT NAME:Caleb
+// DATE: Chun
 
 public class Main 
 {
@@ -7,15 +7,16 @@ public class Main
 	{
 
 		/***** DECLARATION SECTION *****/
-
+	int days = 2, month = 2, year = 2002;
 		/***** INITIALIZATION SECTION *****/
 		
 		/***** INPUT SECTION *****/
 		// N/A (no input for this lab)
 		
 		/***** CALCULATION & PROCESSING SECTION *****/
-
+int jdn = days + (153*month+2)/5 + 365*year + year*0.25 - year*0.01 + year/400 - 32045;
 		
 		/***** OUTPUT SECTION *****/
+		System.out.println("Julian Date Number: " + jdn);
   }
 }
