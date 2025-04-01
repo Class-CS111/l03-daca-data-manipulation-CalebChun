0814719081dd06a0eeebbc38efa71677f3fe7e53
CalebChun <1024656@oside.us>
2025-04-01 09:24:55 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,16 +7,16 @@ public class Main
 	{
 
 		/***** DECLARATION SECTION *****/
-	int dayToday = 2, monthToday = 2, yearToday = 2020;
-	int dayBday = 8, monthBday = 26, yearBday = 1918;
+	double dayToday = 2, monthToday = 2, yearToday = 2020;
+	double dayBday = 8, monthBday = 26, yearBday = 1918;
 		/***** INITIALIZATION SECTION *****/
 		
 		/***** INPUT SECTION *****/
 		// N/A (no input for this lab)
 		
 		/***** CALCULATION & PROCESSING SECTION *****/
-int jdnToday = dayToday + (153*monthToday+2)/5 + 365*yearToday + yearToday*0.25 - yearToday*0.01 + yearToday/400 - 32045;
-int jdnBday = dayBday + (153*monthBday+2)/5 + 365*yearBday + yearBday*0.25 - yearBday*0.01 + yearBday/400 - 32045;
+double jdnToday = dayToday + (153*monthToday+2)/5 + 365*yearToday + yearToday*0.25 - yearToday*0.01 + yearToday/400 - 32045;
+double jdnBday = dayBday + (153*monthBday+2)/5 + 365*yearBday + yearBday*0.25 - yearBday*0.01 + yearBday/400 - 32045;
 		
 		/***** OUTPUT SECTION *****/
 		System.out.println("Julian Day Number for today's date, 2/2/2020, is " + jdnToday);
